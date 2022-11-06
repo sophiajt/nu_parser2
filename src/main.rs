a52dc017adbad555143bd8fff8371d7d725cf5ca
kubouch <kubouch@gmail.com>
2022-11-06 22:55:11 +0100

    Start testing framework prototype

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ mod shell_error;
 
 use std::io::Read;
 
+#[cfg(test)]
+use std::fs::File;
+#[cfg(test)]
+use std::path::PathBuf;
+
+use rstest::rstest;
+
 use parser::Parser;
 
 fn main() -> std::io::Result<()> {
@@ -25,11 +32,51 @@ fn main() -> std::io::Result<()> {
 
         let result = parser.delta;
 
-        result.print();
+        result.print(&mut None);
         for error in parser.errors {
             error.print(&filename, &source);
         }
     }
 
     Ok(())
+}
+
+#[rstest]
+#[case("expression/sample6.nu", "expression/sample6.out")]
+fn test_sample(#[case] sample_file: &str, #[case] out_file: &str) {
+    // Parser output
+    let sample_file = PathBuf::from("tests")
+        .canonicalize()
+        .unwrap()
+        .join(sample_file);
+
+    let span_offset = 0;
+    let mut source = vec![];
+
+    let mut f = std::fs::File::open(&sample_file).unwrap();
+    f.read_to_end(&mut source).unwrap();
+
+    let mut parser = Parser::new(&source, span_offset, 0);
+
+    parser.parse();
+
+    let result = parser.delta;
+
+    let mut buffer = String::new();
+    result.print(&mut Some(&mut buffer));
+
+    // Expected output
+    let out_file = PathBuf::from("tests")
+        .canonicalize()
+        .unwrap()
+        .join(out_file);
+
+    let mut expected = String::new();
+
+    File::open(out_file)
+        .unwrap()
+        .read_to_string(&mut expected)
+        .unwrap();
+
+    assert_eq!(buffer.trim(), expected.replace("\r\n", "\n").trim());
 }
